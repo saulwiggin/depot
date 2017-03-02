ddfa14d80900fc0239af7c3d4e1a04d6a1c5d94e
saul wiggin <saulwiggin@googlemail.com>
2017-03-02 09:55:43 +0000

    edit migration

--- db/migrate/20150727094443_add_order_id_to_line.rb
+++ db/migrate/20150727094443_add_order_id_to_line.rb
@@ -1,6 +1,10 @@
 class AddOrderIdToLine < ActiveRecord::Migration
   def change
-   # add_column :lines, :item, :string
+    add_column :lines, :item, :string
+    update %{
+      UPDATE users
+      SET string = TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE())
+    }
    # add_column :lines, :order_id, :integer
   end
 end
